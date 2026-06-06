7a6a02ec473ea7b479dab40707adef25fac07a31
AurevaFitness <mtrofinst@gmail.com>
2026-06-06 15:30:59 -0400

    Add files via upload

--- workoutImages.js
+++ workoutImages.js
@@ -1,6 +1,35 @@
+import w1  from './w1.jpg'
+import w2  from './w2.jpg'
+import w3  from './w3.jpg'
+import w4  from './w4.jpg'
+import w5  from './w5.jpg'
+import w6  from './w6.jpg'
+import w7  from './w7.jpg'
+import w8  from './w8.jpg'
+import w9  from './w9.jpg'
+import w10 from './w10.jpg'
+import w11 from './w11.jpg'
+import w12 from './w12.jpg'
+import w13 from './w13.jpg'
+import w14 from './w14.jpg'
+import w15 from './w15.jpg'
+import w16 from './w16.jpg'
+import w17 from './w17.jpg'
+import w18 from './w18.jpg'
 import w19 from './w19.jpg'
+import w20 from './w20.jpg'
+import w21 from './w21.jpg'
+import w22 from './w22.jpg'
+import w23 from './w23.jpg'
+import w24 from './w24.jpg'
+import w25 from './w25.jpg'
+import w26 from './w26.jpg'
+import w27 from './w27.jpg'
+import w28 from './w28.jpg'
 
-// Add each image here as you upload them to GitHub (w1.jpg … w28.jpg)
 export const WORKOUT_IMAGES = {
-  19: w19,
+  1:w1,   2:w2,   3:w3,   4:w4,   5:w5,   6:w6,   7:w7,
+  8:w8,   9:w9,   10:w10, 11:w11, 12:w12, 13:w13, 14:w14,
+  15:w15, 16:w16, 17:w17, 18:w18, 19:w19, 20:w20, 21:w21,
+  22:w22, 23:w23, 24:w24, 25:w25, 26:w26, 27:w27, 28:w28
 }
